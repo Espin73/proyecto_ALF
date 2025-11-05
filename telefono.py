1fe8edfa12bf80affb32653da4471774a4428f01
Espin <espinmartinezjuanantonio@gmail.com>
2025-11-05 12:53:28 +0100

    telefono terminado

--- telefono.py
+++ telefono.py
@@ -1,6 +1,16 @@
 import regex as re
 
 
-patron_telefono = r'\d{9}'
+patron_telefono = r"^\s*(\d{9})\s*$"
 
 er_telefono = re.compile(patron_telefono)
+
+def comprobar_telefono(telefono):
+
+    coincidencia = er_telefono.fullmatch(telefono)
+
+    if coincidencia:
+        num = coincidencia.group(1)
+        return "+34" + num
+    else:
+        return None
